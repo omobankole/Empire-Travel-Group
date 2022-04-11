2a3c687aef6b1e4de594172e32c0756442fed7eb
omobankole <oluwabankyusuf1@gmail.com>
2022-04-11 17:53:55 +0100

    fixed calendar arrows

--- src/components/Landing/Calender/calender.js
+++ src/components/Landing/Calender/calender.js
@@ -1,27 +1,27 @@
 import classes from "./calender.module.css";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
-import { DateRange } from 'react-date-range';
-import {useState} from 'react'
+import { DateRange } from "react-date-range";
+import { useState } from "react";
 
 const Calender = () => {
   const [state, setState] = useState([
     {
       startDate: new Date(),
       endDate: null,
-      key: 'selection'
-    }
+      key: "selection",
+    },
   ]);
   return (
     <div className={classes.main}>
       <h2>Holiday Schedule Calendar</h2>
       <div className={classes.getNow}>
-      <DateRange
-  editableDateInputs={true}
-  onChange={item => setState([item.selection])}
-  moveRangeOnFirstSelection={false}
-  ranges={state}
-/>
+        <DateRange
+          editableDateInputs={true}
+          onChange={(item) => setState([item.selection])}
+          moveRangeOnFirstSelection={false}
+          ranges={state}
+        />
         <div className={classes.line}></div>
         <div className={classes.down}>
           <p className={classes.tour}>Tour</p>
